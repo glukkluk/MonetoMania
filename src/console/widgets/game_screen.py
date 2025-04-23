52396ff8c1d4b061865d353fac7b11e2ab6b6bc1
glukkluk <glukkluk.dev@gmail.com>
2025-04-23 14:07:49 +0000

    Implement a new layout element
    I add a class CurrentGameState to display information about the state of the game process, namely:
        - the current duration of the game,
        - the selected target player,
        - the countdown of the current player's turn.
    
    Also created an empty class (GameProcess) to display the actual game process.
    
    And borders for containers for visibility.

--- src/console/widgets/game_screen.py
+++ src/console/widgets/game_screen.py
@@ -1,5 +1,21 @@
-from textual.containers import Container
+from textual.widgets import Label, Digits, ProgressBar
+from textual.containers import Container, Vertical, Grid, Center
 
 
-class GameScreenWidget(Container):
+class CurrentGameState(Grid):
+    def compose(self):
+        yield Label("4:05")
+        yield Container()
+        with Vertical():
+            yield Center(Digits("56", id="timeout"))
+            yield ProgressBar()
+
+
+class GameProcess(Container):
     pass
+
+
+class GameScreenWidget(Grid):
+    def compose(self):
+        yield CurrentGameState()
+        yield GameProcess()
